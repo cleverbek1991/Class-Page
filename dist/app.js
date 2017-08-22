6c4f4d4f2c0486ad9478758282b04cc7b093d0f2
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-08-22 09:56:09 -0500

    Populated. Ugly as sin, but populating.

--- dist/app.js
+++ dist/app.js
@@ -10,10 +10,28 @@ let buildCards = () => {
 	bios.getBios()
 	.then(promiseData => {
 		console.log(promiseData);
+		let htmlString= `<div class="container"><div class="row">`;
+		let c = 0;
 		for (var thing in promiseData){
-			console.log(promiseData[thing]);
-		}
-	});
+			c++;
+			// if (c%3 === 0){
+			// 	htmlString += `</div>
+			// 					<div class="row">
+			// 					<div class="col-md-4">
+			// 						<img class="bioPic" src="../${promiseData[thing].picture}">
+			// 					</div>`;
+			// } else {
+				console.log(promiseData[thing]);
+				htmlString += `<div class="col-md-4">
+									<img class="bioPic" src="../${promiseData[thing].picture}">
+									<h2>${promiseData[thing].first} ${promiseData[thing].last}</h2>
+								</div>`;
+			}
+			htmlString += `</div></div>`;
+			console.log("string");
+			console.log("htmlString", htmlString);
+			$("#cards").append(htmlString);
+		});
 };
 
 buildCards();
