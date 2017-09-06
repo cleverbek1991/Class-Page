3bf2018b87c94b70bebdb409e27a35c11a1b89c0
Adam Reidelbach <adamreidelbach@gmail.com>
2017-09-06 11:01:20 -0500

    still working

--- dist/app.js
+++ dist/app.js
@@ -15,10 +15,32 @@ let buildCards = () => {
 		for (var thing in promiseData){
 			c++;
 			console.log(promiseData[thing]);
-			htmlString += `<div class="col-md-4">
-								<img class="bioPic" src="../${promiseData[thing].picture}">
-								<h2>${promiseData[thing].first} ${promiseData[thing].last}</h2>
-							</div>`;
+            htmlString += `<div class="col-md-4">
+                                <div data-toggle="modal" data-target="#bioModal${promiseData[thing].first}">
+                                    <img class="bioPic" src="../${promiseData[thing].picture}">
+                                    <h2>${promiseData[thing].first} ${promiseData[thing].last}</h2>
+                                </div>
+                            </div>
+                            <div class="modal fade" id="bioModal${promiseData[thing].first}" tabindex="-1" role="dialog" aria-labelledby="bioModalLabel">
+                              <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                     <h4 class="modal-title" id="myModalLabel"><div id="studentName">${promiseData[thing].first} ${promiseData[thing].last}</div></h4> 
+                                  </div>
+                                  <div class="modal-body">
+                                    <img src="images/default-image.png">
+                                    <div id="studentBio">${promiseData[thing].bio}</div>
+                                    <a href class="bioLinks">${promiseData[thing].github}</a><br>
+                                    <a href class="bioLinks">${promiseData[thing].linkedin}</a><br>
+                                    <a href class="bioLinks">${promiseData[thing].personal}</a>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>`;
 			}
 			htmlString += `</div></div>`;
 			console.log("string");
